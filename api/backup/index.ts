12c7f11658c8ee23b7ca12e02ccc47c47b15f3ce
Rastavich <isaac.hauser@live.com.au>
2020-12-21 22:35:09 +1000

    Make recipe_backups more robust

--- api/backup/index.ts
+++ api/backup/index.ts
@@ -8,12 +8,19 @@ export default async (req: NowRequest, res: NowResponse) => {
     try {
         const db = await connectToDatabase();
         const collection = await db.collection("recipes");
-        const collectionBackup = await db.collection("recipe_backups");
-
-        // No query so return all
         const recipes = await collection.find({}).toArray(); 
-        const recipesBackup = await collectionBackup.insertMany(recipes);
-        res.status(200).json( {recipesBackup} );
+
+        const collectionBackup = await db.collection("recipe_backups");
+        if (recipes != "") {
+            collectionBackup.drop();      
+            const recipesBackup = await collectionBackup.insertMany(
+                recipes
+            );
+            res.status(200).json( {recipesBackup} );
+        } else {
+            res.status(400).json({ status: "Error - cannot drop backup as recipes is empty"});
+        }
+        
         
     } catch (err) {
         res.status(500).json({ status: "Error" + err });
